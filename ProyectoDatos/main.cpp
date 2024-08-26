4857fa614ec8486d62acb5c20481795d95650d45
Anthony Li Perera <anthony.li.perera@est.una.ac.cr>
2024-08-26 17:35:02 -0600

    Prueba^5 Bians lo tomo personal

--- ProyectoDatos/main.cpp
+++ ProyectoDatos/main.cpp
@@ -5,6 +5,7 @@ int main() {
 	std::cout << "-------------------" << std::endl;
 	std::cout << "Hola, como esta Bians?" << std::endl;
 	std::cout << "Bien y usted?" << std::endl;
+	std::cout << "Bien, gracias." << std::endl;
 	std::cout << "-------------------" << std::endl;
 
 	return 0;
