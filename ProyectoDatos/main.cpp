1d938482ab20f6bb49cd7b8ad59735bf9acfca4b
Anthony Li Perera <anthony.li.perera@est.una.ac.cr>
2024-08-26 17:30:38 -0600

    Update main.cpp

--- ProyectoDatos/main.cpp
+++ ProyectoDatos/main.cpp
@@ -4,6 +4,7 @@ int main() {
 	
 	std::cout << "-------------------" << std::endl;
 	std::cout << "Hola, como esta Bians?" << std::endl;
+	std::cout << "Bien y usted?" << std::endl;
 	std::cout << "-------------------" << std::endl;
 
 	return 0;
