2b93c8118e7f7c50caa77e88db36656945453f16
SEBASTIAN ALVAREZ GOMEZ <sebastian.alvarez.gomez@est.una.ac.cr>
2024-08-26 17:36:36 -0600

    mensajito

--- ProyectoDatos/main.cpp
+++ ProyectoDatos/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+using namespace std;
 
 int main() {
 	
@@ -7,6 +8,7 @@ int main() {
 	std::cout << "Bien y usted?" << std::endl;
 	std::cout << "Bien, gracias." << std::endl;
 	std::cout << "-------------------" << std::endl;
+	cout << "Me defeco en github y en Anthony" << endl;
 
 	return 0;
 }
