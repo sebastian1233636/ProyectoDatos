7a438b15d701725cc8833a8996bab4c349880e08
FIORELLA MOREIRA SOSA <fiosoza15@gmail.com>
2024-09-27 22:34:05 -0600

    documentacion interna

--- ProyectoDatos/AdminPestañas.h
+++ ProyectoDatos/AdminPestañas.h
@@ -2,19 +2,23 @@
 #define ADMINPESTAÑAS_H
 
 #include"Pestaña.h"
+// Estructura para crear una lista doblemente enlazada que almacena pestañas.
 
 struct NodoPest {
-	Pestaña* pestaña;
-	NodoPest* siguiente;
-	NodoPest* anterior;
+	Pestaña* pestaña;// Apuntador a una instancia de Pestaña.
+	NodoPest* siguiente;// Apuntador al siguiente nodo en la lista.
+	NodoPest* anterior;// Apuntador al nodo anterior en la lista.
 };
+// Clase que administra una lista doblemente enlazada de pestañas.
+// Administra operaciones como la inserción de pestañas, exploración del historial, búsqueda de favoritas, entre otras.
 
 class AdminPestañas {
 private:
 	NodoPest* tail;//Primero
 	NodoPest* head;//Ultimo
-	int tam;
+	int tam; // Tamaño actual de la lista de pestañas (cantidad de pestañas).
 public:
+<<<<<<< HEAD
 	//Construtor y Destructor
 	AdminPestañas();
 	~AdminPestañas();
@@ -22,10 +26,24 @@ public:
 	//Metodos de lista
 	NodoPest* getTail();
 	NodoPest* getHead();
+=======
+	// Constructores y destructor.
+	AdminPestañas();
+	~AdminPestañas();
+	// GETTERS
+	NodoPest* getTail();
+	NodoPest* getHead();
+	// Inserta una nueva pestaña al inicio de la lista (actualiza el tail).
+>>>>>>> documentacion interna
 	void InsertarPrimero(Pestaña*);
+	// Recorre y muestra todas las pestañas en el historial desde la más reciente (tail) hasta la más antigua (head).
 	void ExplorarHistorialPestañas();
+	// Busca pestañas que están marcadas como favoritas y las muestra.
 	void BuscarFavorito();
+	// Devuelve el número total de pestañas actualmente almacenadas en la lista.
 	int contadorPestañas();
+	// Muestra un menú con opciones de administración de pestañas. 
+	// Recibe un nodo actual para facilitar la navegación entre las pestañas.
 	void menuAdminPestañas(NodoPest* actual);
 	PaginaWeb* buscaPaginaWeb(string);
 
--- ProyectoDatos/PaginaWeb.h
+++ ProyectoDatos/PaginaWeb.h
@@ -9,6 +9,7 @@
 #include<fstream>
 
 using namespace std;
+<<<<<<< HEAD
 class PaginaWeb {
 private:
 	string URL;
@@ -23,10 +24,26 @@ public:
 	PaginaWeb();
 
 	//GETTERS
+=======
+//Representa una página web con atributos como URL, título, estado de marcador, y tiempo de ingreso.
+class PaginaWeb{
+private:
+	string URL;// Almacena la URL de la página web.
+	string Titulo; // Almacena el título de la página web.
+	bool marcador;// Indica si la página está marcada como favorita.
+	bool mostrada;// Indica si la página ya ha sido mostrada (para evitar mostrarla repetidamente en ciertas operaciones).
+	time_t tiempoIngreso;// Almacena el tiempo de ingreso (creación) de la página web.
+public:
+	// Constructores y destructor.
+	PaginaWeb(string, string);
+	PaginaWeb();
+	// GETTERS
+>>>>>>> documentacion interna
 	string getURL();
 	string getTitulo();
 	string getMarcadorPersonal();
 	bool getMarcador();
+<<<<<<< HEAD
 	time_t getTiempo();
 
 	//SETTERS
@@ -36,16 +53,33 @@ public:
 	void setTiempo(time_t);
 
 	//Metodos adicionales
+=======
+	// Método que indica si la página ya ha sido mostrada (para evitar mostrar duplicados).
+>>>>>>> documentacion interna
 	bool yaMostrada();
+	// Marca la página como mostrada.
 	void marcarComoMostrada();
+	// Muestra la información de la página web (URL y título).
 	void MostrarPaginaWeb();
+	// Marca la página como favorita.
 	void PonerMarcador();
+	// Quita el marcador de la página (la desmarca como favorita).
 	void QuitarMarcador();
+<<<<<<< HEAD
 	string mostrarTiempo();
 	
 	//archivos
 	void guardarPaginaWeb(ofstream&);
 	PaginaWeb* leerPaginaWeb(ifstream&);
 
+=======
+	// Devuelve el tiempo de ingreso de la página web.
+
+	time_t getTiempo();
+	// Devuelve el tiempo de ingreso de la página en formato legible.
+	string mostrarTiempo();
+	// Asigna un tiempo de ingreso específico a la página web.
+	void setTiempo(time_t);
+>>>>>>> documentacion interna
 };
 #endif
